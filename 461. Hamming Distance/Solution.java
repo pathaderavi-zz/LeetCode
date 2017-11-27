325600ec4783858d29b6d70756dfad212dceb3ef
Ravikiran Pathade <pathaderavi@gmail.com>
2017-11-27 12:20:02 -0500

    import java.io.*;

--- 461. Hamming Distance/Solution.java
+++ 461. Hamming Distance/Solution.java
@@ -1,43 +1,38 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
- 
-        System.out.println(lengthOfLongestSubstring("c"));
-        }
-    
-    public static int lengthOfLongestSubstring(String s){
-        
-        HashSet<Character> subString = new HashSet();
-        int j = -1 ;
-        int current = 0;
-
-        
-        for(int i = 0 ; i< s.length() ;){
-        
-        if(!subString.contains(s.charAt(i)) && j <= i){
-            
-            subString.add(s.charAt(i));
-            i++;
-            
-        }else{
-            current = Math.max(current,subString.size());
-            if(j==-1){
-                j=0;
-            }
-            subString.remove(s.charAt(j));
-            
-            j++;
-        }
-        if(i==s.length() && !subString.isEmpty()){
-            current = Math.max(current, subString.size());
-        }
-        
-        }
-    return current;
+    System.out.println(hammingDistance(19,2));
         
     }
-
+    
+   
+        public static int hammingDistance(int x, int y) {
+        int a = Math.max(x, y);
+        int b = Math.min(x, y);
+        
+        String xB = Integer.toBinaryString(a);
+        String yB = Integer.toBinaryString(b);
+        
+        String d = "";
+        for(int i = 0 ; i< (xB.length()-yB.length());i++){
+            
+            d = d+"0";
+        }
+        yB = d+yB;
+        int distance=0;
+        for(int i = 0;i<xB.length();i++){
+            if(!(xB.charAt(i)==(yB.charAt(i)))){
+                distance = distance +1;
+            }
+        }
+        
+        System.out.println(xB+" "+yB);
+  return distance;      
+            
+    }
 }
